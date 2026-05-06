9542b024acc1502860e5b92a22576d21c75deaee
Prachi Patro <prachipatro11@gmail.com>
2026-05-06 19:39:51 +0530

    Update data_fetch.py

--- data_fetch.py
+++ data_fetch.py
@@ -1,10 +1,14 @@
 import yfinance as yf
 
 def get_stock_data(ticker):
-    data = yf.download(ticker, period="1mo", interval="1d")
+    try:
+        stock = yf.Ticker(ticker)
+        data = stock.history(period="1mo")
 
-    # HANDLE EMPTY DATA
-    if data.empty:
+        if data is None or data.empty:
+            return None
+
+        return data
+
+    except Exception:
         return None
-
-    return data
