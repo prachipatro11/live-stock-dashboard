abe892604ec7d7cac099bf362ca2b9f96be45427
Prachi Patro <prachipatro11@gmail.com>
2026-05-06 19:07:38 +0530

    Update data_fetch.py

--- data_fetch.py
+++ data_fetch.py
@@ -2,4 +2,9 @@ import yfinance as yf
 
 def get_stock_data(ticker):
     data = yf.download(ticker, period="1mo", interval="1d")
+
+    # HANDLE EMPTY DATA
+    if data.empty:
+        return None
+
     return data
